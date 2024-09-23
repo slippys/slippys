f05216141f648c3b35ce2340e38ae8f417fbfb77
Stephen McGuckin <stephendmcguckin@gmail.com>
2024-09-22 18:49:34 -0700

    adds frontend

--- packages/deployment/src/iac/index.ts
+++ packages/deployment/src/iac/index.ts
@@ -1,26 +1,19 @@
 #!/usr/bin/env node
 import {App} from 'aws-cdk-lib';
-
 import {RestApiStack} from 'rest-api';
-import {CertificateStack} from './certificate.stack.js';
-import {domainName, subdomain, env, appName} from 'config';
+import {domainName, env, appName, apiSubDomain, frontendSubDomain} from 'config';
+import {FrontendStack} from 'frontend';
 
 const app = new App();
 
-const certStack = new CertificateStack(app, `${appName}-certificate-stack`, {
-    env: {
-        ...env,
-        region: 'us-east-1'
-    },
-    crossRegionReferences: true,
+new FrontendStack(app, `${appName}-frontend-stack`, {
+    env,
     domainName,
-    subdomain,
+    subdomain: frontendSubDomain,
 });
 
-export const stack = new RestApiStack(app, `${appName}-rest-api-stack`, {
+new RestApiStack(app, `${appName}-rest-api-stack`, {
     env,
-    crossRegionReferences: true,
-    certificate: certStack.cert,
     domainName,
-    subdomain,
+    subdomain: apiSubDomain,
 });
